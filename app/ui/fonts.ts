acc90d190c6efc498c94dfe648d21e78ef81b968
rizkymuliap <perdanarizkymulia162@gmail.com>
2025-03-17 01:00:20 +0700

    Third Commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import { Inter, Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
+import { Inter, Lusitana } from 'next/font/google'; 
 
-export const lusitana = Lusitana({ weight: "700" });
+export const inter = Inter({ subsets: ['latin'] }); 
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'], }); 
